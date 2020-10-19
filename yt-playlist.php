ee7de4f4ee33cfe8bbaae60acf995e826d230abb
Ankit kumar <cs.ankitprajapati@gmail.com>
2020-10-19 22:07:17 +0530

    added css,and js files

---
+++ yt-playlist.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       YT Playlist
+ * Plugin URI:        http://codingbirdsonline.com/plugins/yt-playlist
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Author:            Ankit Prajapati
+ * Author URI:        https://ankit.codingbirdsonline.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       yt-playlist
+ * Domain Path:       /languages
+ */
+
+ 
