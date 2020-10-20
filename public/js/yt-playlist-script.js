7b700914538765cc542c8e4609447f3e2877c8df
Aniket uttam <aniketuttam11@gmail.com>
2020-10-20 23:58:57 +0530

    Update yt-playlist-script.js

--- public/js/yt-playlist-script.js
+++ public/js/yt-playlist-script.js
@@ -1,8 +1,14 @@
-
+/*
+This function is used to play video by clicking play button
+*/
 function ytPlayVideo()
 {
 
 }
+
+/*
+This function is used to pause video by clicking pause button
+*/
 
 function ytCloseVideo()
 {
